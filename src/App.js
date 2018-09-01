74b1dfc0a216e6b79cbcdc64d312e7fd31e98c0c
rudenate3 <nathan.odin@gmail.com>
2018-09-01 14:55:14 -0400

    mp-5 Add Redux and Thunk
    -(feature) Add Redux and Thunk and initial config
    -(deps) Install redux, react-redux, redux-thunk
    
    Closes: #5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 import Landing from './components/landing/Landing'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="app">
-          <Route exact path="/" component={Landing} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="app">
+            <Route exact path="/" component={Landing} />
+          </div>
+        </Router>
+      </Provider>
     )
   }
 }
