f4f40df759afcfc8efe6f41cdd669d4b26ae0ee4
rudenate3 <nathan.odin@gmail.com>
2018-09-01 21:28:52 -0400

    mp-46 Add About page component
    -(feature) Add About page component
    
    Closes: #46

---
+++ src/components/about/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../common/ui/PageHeader'
+
+export default () => {
+  return (
+    <div>
+      <PageHeader content="About" />
+
+      <p>
+        Meal Prep is an app developed to act as a recipe book with a focus on
+        meal prepping.
+      </p>
+    </div>
+  )
+}
