dff1e5547c807e3dca0098eb93516445c5379953
rudenate3 <nathan.odin@gmail.com>
2018-09-01 21:25:03 -0400

    mp-7 Add Home component
    -(feature) Add Home component
    
    Closes: #7

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PageHeader from '../common/ui/PageHeader'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <PageHeader content="Home" />
+      </div>
+    )
+  }
+}
