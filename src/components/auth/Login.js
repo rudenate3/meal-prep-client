84a8d06340c5e52b7af6d6dbd037759d4335a4df
rudenate3 <nathan.odin@gmail.com>
2018-09-04 02:56:36 -0400

    mp-58 Redirect auth user from Landing page
    -(feature) Redirect authorized user from Landing page
    -(refactor) Refactor Login component to redirect to /
    
    Closes: #58

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -24,13 +24,13 @@ class Login extends Component {
 
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/home')
+      this.props.history.push('/')
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push('/home')
+      this.props.history.push('/')
     }
 
     if (nextProps.errors) {
